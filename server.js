a093123502eef7cdf09b2fb8e1a330ae82babc29
sanmeranam@gmail.com <>
2016-08-05 22:24:00 +0530

    Theme package modified, api rest implemented, ux changed

--- server.js
+++ server.js
@@ -17,17 +17,18 @@ var GLOBAL = require('./core/GLOBAL');
 var app = express();
 
 GLOBAL.sessionStore = new session.MemoryStore();
+var oConfig = JSON.parse(require("fs").readFileSync('config.json', 'utf8'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'applications'));
 app.set('view engine', 'jade');
 
 app.use(require('subdomain')({
-    base: "localhost",
+    base: oConfig.server.domain,
     prefix: 'wildcard'
 }));
+app._port=oConfig.server.port;
 
-var oConfig = JSON.parse(require("fs").readFileSync('config.json', 'utf8'));
 var oDBConnect = new _cDBConnect(oConfig.db.url);
 
 
@@ -59,19 +60,19 @@ app.use(function (req, res, next) {
 });
 
 
-//app.use('/wildcard/admin/_self', express.static(path.join(__dirname, 'applications/admin/client')));
-//app.use('/wildcard/admin/_static', express.static(path.join(__dirname, 'public')));
-//app.use('/wildcard/admin', _mRouteAdmin);
+app.use('/wildcard/admin/_self', express.static(path.join(__dirname, 'applications/admin/client')));
+app.use('/wildcard/admin/_static', express.static(path.join(__dirname, 'public')));
+app.use('/wildcard/admin', _mRouteAdmin);
 
 app.use('/wildcard/api', _mRouteApi);
 
 
-app.use('/_self', express.static(path.join(__dirname, 'applications/admin/client')));
-app.use('/_static', express.static(path.join(__dirname, 'public')));
-//app.use('/_site', express.static(path.join(__dirname, 'applications/website/client')));
+//app.use('/_self', express.static(path.join(__dirname, 'applications/admin/client')));
+//app.use('/_static', express.static(path.join(__dirname, 'public')));
+app.use('/_site', express.static(path.join(__dirname, 'applications/website/client')));
 
-//app.use('/', _mRouteWebsite);
-app.use('/', _mRouteAdmin);
+app.use('/', _mRouteWebsite);
+//app.use('/', _mRouteAdmin);
 
 
 // catch 404 and forward to error handler
