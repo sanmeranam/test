b7d2082954bf265743c88fa727733daecbbf7f8a
sanmeranam@gmail.com <>
2016-09-05 01:06:48 +0530

    App access ui url created trial

--- applications/app/server/route.js
+++ applications/app/server/route.js
@@ -9,4 +9,8 @@ router.get('/', function (req, res, next) {
     res.render('./app/views/index', {title: 'Forms 1.0'});
 });
 
+router.get('/newform', function (req, res, next) {
+    res.render('./app/views/newform', {title: 'Forms 1.0'});
+});
+
 module.exports = router;
